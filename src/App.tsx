d015cce68523e5ca0f3786d5e8bd337de230dc9c
DESKTOP-6JV755H\taeseong <taese0ng@naver.com>
2023-06-26 23:44:57 +0900

    feat: router 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Fragment } from 'react';
+
 import { Global } from '@emotion/react';
+import { Outlet } from 'react-router-dom';
 
 import { reset } from './styles';
 
 function App() {
 	return (
-		<>
+		<Fragment>
 			<Global styles={reset} />
-		</>
+			<Outlet />
+		</Fragment>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import router from '@routes/index';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
 
