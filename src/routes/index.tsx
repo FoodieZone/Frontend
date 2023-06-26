5956fe35a8516733cbf81c8ae945bac0ef5d6115
DESKTOP-6JV755H\taeseong <taese0ng@naver.com>
2023-06-26 23:55:41 +0900

    feat: router 기본구조 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,23 @@
+import type { ReactNode } from 'react';
+
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from '~/App';
 
-const router = createBrowserRouter([
+interface RouterBase {
+	id: string;
+	path: string;
+	element: ReactNode;
+}
+
+const routeData: RouterBase[] = [
 	{
+		id: 'Home',
 		path: '/',
 		element: <App />,
 	},
-]);
+];
+
+const router = createBrowserRouter(routeData.map((route) => ({ path: route.path, element: route.element })));
 
 export default router;
