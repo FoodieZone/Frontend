b11df4db53387dbdc7be791346bef46e05713de9
gunwoongPark <bung1438@gmail.com>
2023-08-06 21:52:42 +0900

    feat: 최대, 최소 width 설정

--- src/components/Layout/index.styles.ts
+++ src/components/Layout/index.styles.ts
@@ -1,5 +1,12 @@
 import styled from '@emotion/styled';
 
 export const S = {
-	Container: styled.main``,
+	Container: styled.main`
+		width: 100%;
+		height: 100vh;
+		min-width: 375px;
+		max-width: 768px;
+		margin: 0 auto;
+		background-color: blue;
+	`,
 };
