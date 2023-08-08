72c60e958d60f312e7f458decb9e4d61144f308f
gunwoongPark <bung1438@gmail.com>
2023-08-09 02:35:58 +0900

    feat: 컨벤션 적용 & eslint 수정

--- src/components/BaseLayout/index.tsx
+++ src/components/BaseLayout/index.tsx
@@ -1,7 +1,15 @@
 import type { PropsWithChildren } from 'react';
 
-import { S } from './index.styles';
+import styled from '@emotion/styled';
 
-const BaseLayout = ({ children }: PropsWithChildren) => <S.Container>{children}</S.Container>;
+const BaseLayout = ({ children }: PropsWithChildren) => <Container>{children}</Container>;
+
+const Container = styled.main`
+	width: 100%;
+	height: 100vh;
+	min-width: 375px;
+	max-width: 768px;
+	margin: 0 auto;
+`;
 
 export default BaseLayout;
