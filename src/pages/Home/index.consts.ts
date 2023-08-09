cb6ec1f32fbef6f9a1a270ab21772aba4c61ed05
gunwoongPark <bung1438@gmail.com>
2023-08-09 18:46:43 +0900

    feat: consts 파일 생성 및 button 구현

---
+++ src/pages/Home/index.consts.ts
@@ -0,0 +1,2 @@
+export const TITLE = `Welcome\nFoodie Zone`;
+export const BUTTON_TEXT = '시작하기';
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,43 @@
-const HomePage = () => <>homepage</>;
+import styled from '@emotion/styled';
+
+import { BUTTON_TEXT, TITLE } from './index.consts';
+
+const HomePage = () => (
+	<Container>
+		<Title>{TITLE}</Title>
+
+		<StartButton>{BUTTON_TEXT}</StartButton>
+	</Container>
+);
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
+const Title = styled.span`
+	margin-top: 155px;
+	white-space: pre-line;
+	color: #222;
+	font-size: 36px;
+	font-weight: 800;
+	line-height: 44px;
+`;
+
+const StartButton = styled.button`
+	width: 295px;
+	height: 50px;
+	border-radius: 12px;
+	box-shadow: 0px 10px 20px 0px rgba(6, 0, 101, 0.2);
+	position: fixed;
+	bottom: 47px;
+	outline: none;
+	background-color: #5887f6;
+	color: #fff;
+	font-size: 18px;
+	font-weight: 600;
+	border: none;
+	cursor: pointer;
+`;
 
 export default HomePage;
