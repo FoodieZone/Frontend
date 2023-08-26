5c2e884711328576ecc0df0e36926e413399e536
gunwoongPark <bung1438@gmail.com>
2023-08-27 04:48:45 +0900

    feat: 임시 로딩처리

--- src/pages/WorldCupResult.tsx
+++ src/pages/WorldCupResult.tsx
@@ -6,20 +6,31 @@ import Dumplings from 'assets/images/13 Dumplings.jpg';
 import type { ResultType } from '~/interfaces/result';
 
 function WorldCupResult() {
+	// FIXME: 임시 로딩 처리
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [result, setResult] = useState<ResultType | null>(null);
 
 	// FIXME: 서버에서 넘어온 값이 들어갈 예정
 	useEffect(() => {
-		setResult(() => ({ id: 1, title: '만두', src: Dumplings }));
+		setTimeout(() => {
+			setResult(() => ({ id: 1, title: '만두', src: Dumplings }));
+			setIsLoading(false);
+		}, 1000);
 	}, []);
 
 	return (
 		<Container>
 			<Title>Final food</Title>
-			{/* TODO: 로딩이 들어갈 예정 */}
-			<Description>당신은 {result?.title ?? ''} 러버입니다!</Description>
-			<FoodImage src={result?.src ?? ''} alt="food-image" />
-			<FoodName>{result?.title ?? ''}</FoodName>
+
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<>
+					<Description>당신은 {result?.title ?? ''} 러버입니다!</Description>
+					<FoodImage src={result?.src ?? ''} alt="food-image" />
+					<FoodName>{result?.title ?? ''}</FoodName>
+				</>
+			)}
 		</Container>
 	);
 }
@@ -59,4 +70,69 @@ const FoodName = styled.span`
 	font-size: 22px;
 	font-weight: 800;
 	line-height: 14px;
+`;
+
+const Spinner = styled.div`
+	font-size: 10px;
+	margin: 50px auto;
+	text-indent: -9999em;
+	width: 11em;
+	height: 11em;
+	border-radius: 50%;
+	background: #000000;
+	background: -moz-linear-gradient(left, #000000 10%, rgba(0, 0, 0, 0) 42%);
+	background: -webkit-linear-gradient(left, #000000 10%, rgba(0, 0, 0, 0) 42%);
+	background: -o-linear-gradient(left, #000000 10%, rgba(0, 0, 0, 0) 42%);
+	background: -ms-linear-gradient(left, #000000 10%, rgba(0, 0, 0, 0) 42%);
+	background: linear-gradient(to right, #000000 10%, rgba(0, 0, 0, 0) 42%);
+	position: relative;
+	-webkit-animation: load3 1.4s infinite linear;
+	animation: load3 1.4s infinite linear;
+	-webkit-transform: translateZ(0);
+	-ms-transform: translateZ(0);
+	transform: translateZ(0);
+
+	&::before {
+		width: 50%;
+		height: 50%;
+		background: #000000;
+		border-radius: 100% 0 0 0;
+		position: absolute;
+		top: 0;
+		left: 0;
+		content: '';
+	}
+	&::after {
+		background: #ffffff;
+		width: 75%;
+		height: 75%;
+		border-radius: 50%;
+		content: '';
+		margin: auto;
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+	}
+	@-webkit-keyframes load3 {
+		0% {
+			-webkit-transform: rotate(0deg);
+			transform: rotate(0deg);
+		}
+		100% {
+			-webkit-transform: rotate(360deg);
+			transform: rotate(360deg);
+		}
+	}
+	@keyframes load3 {
+		0% {
+			-webkit-transform: rotate(0deg);
+			transform: rotate(0deg);
+		}
+		100% {
+			-webkit-transform: rotate(360deg);
+			transform: rotate(360deg);
+		}
+	}
 `;
