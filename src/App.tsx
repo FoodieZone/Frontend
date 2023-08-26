db75fcbf4e5ef9944bcd39de9c7c0b05c0551b1e
gunwoongPark <bung1438@gmail.com>
2023-08-27 05:23:04 +0900

    feat: 페이지로 쉽게 갈 수 있도록 app 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
+import { URL } from './constants';
+
 function App() {
-	return <>App</>;
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column' }}>
+			<Link to={URL.HOME}>HOME</Link>
+			<Link to={URL.MAIN}>MAIN</Link>
+			<Link to={URL.LOCATION_INFORMATION_AGREE}>LOCATION_INFORMATION_AGREE</Link>
+			<Link to={URL.WORLD_CUP.RESULT}>WORLD_CUP.RESULT</Link>
+		</div>
+	);
 }
 
 export default App;
