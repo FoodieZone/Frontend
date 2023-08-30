f80b4ab3e758e2fe61570825dba840371c9c09a0
Yooncastle <dbstjd427@gmail.com>
2023-08-30 23:10:25 +0900

    feat: 토너먼트 구현

--- src/pages/WorldCup.tsx
+++ src/pages/WorldCup.tsx
@@ -1,26 +1,76 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Burger from '@assets/png/Burger.png';
+import CoffeCup from '@assets/png/CoffeCup.png';
 import Pizza from '@assets/png/Pizza.png';
+import Sandwich from '@assets/png/Sandwich.png';
 import styled from '@emotion/styled';
+
+type candidatesFromServerType = { name: string; image: string };
+
+const candidatesFromServer = [
+	{
+		name: '햄버거',
+		image: Burger,
+	},
+	{
+		name: '피자',
+		image: Pizza,
+	},
+	{
+		name: '카페',
+		image: CoffeCup,
+	},
+	{
+		name: '샌드위치',
+		image: Sandwich,
+	},
+];
 
 const WorldCupPage = () => {
 	const [selected, setSelected] = useState<number | undefined>(undefined);
+	const [candidates, setCandidates] = useState<candidatesFromServerType[]>();
+	const [currentRound, setCurrentRound] = useState<number>(1);
+	const [leftIndex, setLeftIndex] = useState<number>(0);
+	const [round, setRound] = useState<number>(0);
 
-	const handleClickCandidate = (index: number) => {
-		setSelected(index);
+	const winners = useRef<candidatesFromServerType[]>([]);
+
+	useEffect(() => {
+		// 서버 요청
+		setCandidates(candidatesFromServer);
+		setRound(candidatesFromServer.length / 2);
+	}, []);
+
+	const handleClickCandidate = (el: candidatesFromServerType, index: number) => {
+		setCurrentRound((prev) => ++prev);
+		setLeftIndex((prev) => prev + 2);
+
+		winners.current.push(el);
+
+		if (winners.current.length === round) {
+			setCandidates(winners.current);
+			setLeftIndex(0);
+			setCurrentRound(0);
+			setRound((prev) => prev / 2);
+		}
 	};
 
 	return (
 		<Container>
-			<Title>Choice Food</Title>
+			<Title>
+				Choice Food {currentRound}/{round}
+			</Title>
 			<CandidatesWrapper>
-				<CandidateLeft onClick={() => handleClickCandidate(0)} selected={selected}>
-					{selected !== 1 && <img src={Burger} alt="Burger" height={200} />}
-				</CandidateLeft>
-				<CandidateRight onClick={() => handleClickCandidate(1)} selected={selected}>
-					{selected !== 0 && <img src={Pizza} alt="Pizza" height={200} />}
-				</CandidateRight>
+				{candidates?.slice(leftIndex, leftIndex + 2).map((el, index) => (
+					<CandidateLeft
+						key={`candidate-${el.name}`}
+						onClick={() => handleClickCandidate(el, index)}
+						selected={selected}
+					>
+						<img src={el.image} alt={el.name} width="100%" style={{ objectFit: 'cover' }} />
+					</CandidateLeft>
+				))}
 			</CandidatesWrapper>
 		</Container>
 	);
@@ -46,16 +96,15 @@ const CandidatesWrapper = styled.div`
 	width: 100%;
 	display: flex;
 	justify-content: space-evenly;
+	gap: 10px;
 
 	margin-top: 100px;
 `;
 
 const Candidate = styled.div`
-	width: 100%;
-
 	text-align: center;
 
-	transition: 1s;
+	transition: 600ms;
 `;
 
 const CandidateLeft = styled(Candidate)<{ selected: number | undefined }>`
