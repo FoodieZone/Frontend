361ee288ad6159d891d0b5eda20c6f70a2748d94
taese0ng <taese0ng@naver.com>
2023-09-08 18:47:20 +0900

    ﻿[FE-18] feat: splash 페이지 생성 webView구분하여 start페이지로의 분기처리

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as LocationAgree } from './LocationAgree';
 export { default as Home } from './Home';
 export { default as Start } from './Start';
 export { default as WorldCupResult } from './WorldCupResult';
+export { default as Splash } from './Splash';
