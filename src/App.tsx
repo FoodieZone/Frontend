fb5704640c38ab5b9208f5b5e7cf2d6bca0ce4ea
taese0ng <taese0ng@naver.com>
2023-09-08 18:47:20 +0900

    [FE-18] feat: splash 페이지 생성 webView구분하여 start페이지로의 분기처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { useIsWebView } from '~/hooks';
+
 function App() {
-	return <>App</>;
+	const isWebView = useIsWebView();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (isWebView) {
+			navigate('/start', { replace: true });
+		} else {
+			navigate('/splash', { replace: true });
+		}
+	}, [isWebView, navigate]);
+
+	return null;
 }
 
 export default App;
