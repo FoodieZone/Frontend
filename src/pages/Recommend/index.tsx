60caf4e0708311ab076cfa7849fccb2273ba69f7
gunwoongPark <bung1438@gmail.com>
2023-09-24 01:43:29 +0900

    feat: 결과 페이지 퍼블리싱 진행

--- src/pages/Recommend/index.tsx
+++ src/pages/Recommend/index.tsx
@@ -1,5 +1,35 @@
+import styled from '@emotion/styled';
+
 function Recommend() {
-	return <></>;
+	return (
+		<Container>
+			<Title>근처 햄버거 인기맛집을 추천드려요 !</Title>
+
+			<FoodName>버거운 버거</FoodName>
+		</Container>
+	);
 }
 
 export default Recommend;
+
+const Container = styled.main`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const Title = styled.span`
+	margin-top: 51px;
+	color: #757682;
+	font-size: 22px;
+	font-weight: 400;
+	line-height: 19px;
+`;
+
+const FoodName = styled.span`
+	margin-top: 46px;
+	color: #1f2a37;
+	font-size: 18px;
+	font-weight: 700;
+	line-height: 27px;
+`;
