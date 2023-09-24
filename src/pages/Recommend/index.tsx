c8fba90f6e6a287b393a731b49ad0da418d4bc89
gunwoongPark <bung1438@gmail.com>
2023-09-24 21:10:57 +0900

    feat: 결과 페이지 퍼블리싱 완료

--- src/pages/Recommend/index.tsx
+++ src/pages/Recommend/index.tsx
@@ -5,17 +5,22 @@ import { Icon } from '~/components/shared';
 function Recommend() {
 	return (
 		<Container>
-			<Title>근처 햄버거 인기맛집을 추천드려요 !</Title>
+			<Contents>
+				<Title>근처 햄버거 인기맛집을 추천드려요 !</Title>
 
-			<FoodName>버거운 버거</FoodName>
+				<FoodInfoWrapper>
+					<FoodName>버거운 버거</FoodName>
+					<FoodImage name="image-burger" height={327} />
+					<FoodLocationWrapper>
+						<Icon name="icon-map-pin" width={18} height={18} />
+						<StoreName>버거운 버거</StoreName>
+						<StoreAddress>서울특별시 특별구 특별로 123길 45</StoreAddress>
+					</FoodLocationWrapper>
+					<Divider />
+				</FoodInfoWrapper>
+			</Contents>
 
-			<FoodImage name="image-burger" height={327} />
-
-			<FoodLocationWrapper>
-				<Icon name="icon-map-pin" width={18} height={18} />
-				<StoreName>버거운 버거</StoreName>
-				<StoreAddress>서울특별시 특별구 특별로 123길 45</StoreAddress>
-			</FoodLocationWrapper>
+			<ShowMoreButton>다른 맛집 더보기</ShowMoreButton>
 		</Container>
 	);
 }
@@ -25,8 +30,14 @@ export default Recommend;
 const Container = styled.main`
 	display: flex;
 	flex-direction: column;
+	justify-content: space-between;
+	height: 100%;
+`;
+
+const Contents = styled.div`
+	display: flex;
+	flex-direction: column;
 	align-items: center;
-	padding: 0 28.5px;
 `;
 
 const Title = styled.span`
@@ -37,8 +48,16 @@ const Title = styled.span`
 	line-height: 19px;
 `;
 
+const FoodInfoWrapper = styled.div`
+	padding: 46px 4.5px 0;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	box-sizing: border-box;
+`;
+
 const FoodName = styled.span`
-	margin-top: 46px;
 	color: #1f2a37;
 	font-size: 18px;
 	font-weight: 700;
@@ -54,7 +73,7 @@ const FoodLocationWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	gap: 8px;
-	width: 100%;
+	width: 318px;
 	margin-top: 12px;
 	padding: 8px 10px;
 	border: 1px solid #e5e7eb;
@@ -72,4 +91,24 @@ const StoreAddress = styled.span`
 	color: #6b7280;
 	font-size: 12px;
 	font-weight: 500;
+`;
+
+const Divider = styled.div`
+	width: 100%;
+	height: 1px;
+	margin-top: 24px;
+	background-color: #e5e7eb;
+`;
+
+const ShowMoreButton = styled.button`
+	height: 50px;
+	margin: 0 28px 32px;
+	border-radius: 12px;
+	outline: none;
+	background-color: #ff5e60;
+	color: #fff;
+	font-size: 15px;
+	font-weight: 600;
+	border: none;
+	cursor: pointer;
 `;
