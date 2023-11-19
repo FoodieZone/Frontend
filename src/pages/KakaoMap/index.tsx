756f1f47e7002eb6686abd578e3409969eacc5a7
gunwoongPark <bung1438@gmail.com>
2023-11-19 12:15:41 +0900

    feat: kakao map 노출

--- src/pages/KakaoMap/index.tsx
+++ src/pages/KakaoMap/index.tsx
@@ -1,4 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef, useState } from 'react';
+
+import useCoords from '~/hooks/useCoords';
+
 declare global {
 	interface Window {
 		kakao: any;
@@ -8,8 +12,46 @@ declare global {
 const { kakao } = window;
 
 function KakaoMapPage() {
-	console.log(kakao);
-	return <div />;
+	const mapRef = useRef<HTMLDivElement>(null);
+
+	const coords = useCoords();
+
+	const [kakaoMap, setKakaoMap] = useState<any>(null);
+
+	useEffect(() => {
+		if (coords === null) {
+			return;
+		}
+
+		const options = {
+			center: new kakao.maps.LatLng(coords.latitude, coords.longitude),
+			level: 4,
+		};
+
+		const map = new kakao.maps.Map(mapRef.current, options);
+
+		setKakaoMap(map);
+	}, [coords, mapRef]);
+
+	useEffect(() => {
+		if (kakaoMap === null) {
+			return;
+		}
+
+		if (mapRef.current === null) {
+			return;
+		}
+
+		const center = kakaoMap.getCenter();
+
+		mapRef.current.style.width = '100%';
+		mapRef.current.style.height = '100%';
+
+		kakaoMap.relayout();
+		kakaoMap.setCenter(center);
+	}, [kakaoMap]);
+
+	return <>{coords === null ? <div>loading...</div> : <div ref={mapRef} />}</>;
 }
 
 export default KakaoMapPage;
