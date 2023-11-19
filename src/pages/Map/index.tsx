ba56fa5fbc82e250ce6b11ae8670061cddac5bf9
gunwoongPark <bung1438@gmail.com>
2023-11-19 17:01:19 +0900

    feat: 내위치 마커 생성(추후 수정)

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -34,7 +34,7 @@ function MapPage() {
 		const map = new kakao.maps.Map(mapRef.current, options);
 
 		setKakaoMap(map);
-	}, [isLocating, latitude, longitude, mapRef, setKakaoMap]);
+	}, [isLocating, latitude, longitude, mapRef]);
 
 	useEffect(() => {
 		if (isNull(kakaoMap)) {
@@ -53,6 +53,17 @@ function MapPage() {
 		kakaoMap.relayout();
 		kakaoMap.setCenter(center);
 	}, [kakaoMap, mapRef]);
+
+	useEffect(() => {
+		if (isNull(kakaoMap)) {
+			return;
+		}
+
+		const currentPositionMarker = new window.kakao.maps.Marker({
+			position: new window.kakao.maps.LatLng(latitude, longitude),
+		});
+		currentPositionMarker.setMap(kakaoMap);
+	}, [kakaoMap, latitude, longitude]);
 
 	if (isLocating) {
 		return (
